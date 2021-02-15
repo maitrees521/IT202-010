55bfbf51f9211d9c8c31140d3f80709f12afaec8
maitrees521 <msp222@njit.edu>
2021-02-15 17:11:29 -0500

    added php flow control files

---
+++ Flow/php_operators.php
@@ -0,0 +1,16 @@
+<?php
+echo "<br>\nPHP [Operators] Exercise [1] <br>\n";
+echo $a = 10 * 5;;
+
+echo "<br>\nPHP [Operators] Exercise [2] <br>\n";
+echo $b = 10 / 2;
+
+$c = 10 / 5;
+$d = 8 - 6;
+
+echo "<br>\nPHP [Operators] Exercise [3] <br>\n";
+echo "var_dump($c == $d)";
+
+echo "<br>\nPHP [Operators] Exercise [4] <br>\n";
+echo "var_dump($a != $b)";
+?>
