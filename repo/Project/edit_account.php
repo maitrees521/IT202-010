e51d4a6b4457505353109eddd5f7b10ecf58c10e
maitrees521 <77853409+maitrees521@users.noreply.github.com>
2021-05-08 17:20:26 -0400

    Update and rename test_edit_account.php to edit_account.php

--- repo/Project/edit_account.php
+++ repo/Project/edit_account.php
@@ -1,20 +1,17 @@
-<<?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<!-- ----------------------------------------------------------------------------------------------------------- -->
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
 if (!has_role("Admin")) {
   //this will redirect to login and kill the rest of this script (prevent it from executing)
   flash("You don't have permission to access this page");
-  die(header("Location: " . getURL("login.php")));
+  die(header("Location: login.php"));
 }
 ?>
-<!-- ----------------------------------------------------------------------------------------------------------- -->
 <?php
 //we'll put this at the top so both php block have access to it
   if(isset($_GET["id"])){
     $id = $_GET["id"];
   }
 ?>
-<!-- ----------------------------------------------------------------------------------------------------------- -->
 <?php
 if(isset($_POST["save"])){
       $account_number = $_POST["account_number"]; //not added to form 
@@ -41,7 +38,6 @@ if(isset($_POST["save"])){
       }
 }
 ?>
-<!-- ----------------------------------------------------------------------------------------------------------- -->
 <?php
 $result = []; 
 if(isset($id)){
@@ -52,20 +48,18 @@ if(isset($id)){
 	$result = $stmt->fetch(PDO::FETCH_ASSOC);
 }
 ?>
-<!-- ----------------------------------------------------------------------------------------------------------- -->
 <form method="POST">
   <label>Account Number</label>
   <input type="number" name="account_number" value="<?php echo $result["account_number"];?>"/>
   <label>Account Type</label>
   <select name="account_type" value="<?php echo $result["account_type"];?>">
-		            <option value="checking" <?php echo ($result["account_type"] == "0"?'selected="selected"':'');?>>checking</option>
+		<option value="checking" <?php echo ($result["account_type"] == "0"?'selected="selected"':'');?>>checking</option>
                 <option value="saving" <?php echo ($result["account_type"] == "2"?'selected="selected"':'');?>>saving</option>
                 <option value="loan" <?php echo ($result["account_type"] == "3"?'selected="selected"':'');?>>loan</option>
                 <option value="world" <?php echo ($result["account_type"] == "4"?'selected="selected"':'');?>>world</option>
 	</select>
   <label>Balance</label> 
-  <input type="number" min="5.00" name="balance" value="<?php echo $result["balance"];?>" />
+  <input type="number" min="10.00" name="balance" value="<?php echo $result["balance"];?>" />
   <input type="submit" name="save" value="Update"/>
 </form>
-<!-- ----------------------------------------------------------------------------------------------------------- -->
 <?php require(__DIR__ . "/partials/flash.php");
