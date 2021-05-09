e99254a0ec3fc74073ecf13c91c109136bd9beef
maitrees521 <77853409+maitrees521@users.noreply.github.com>
2021-05-08 22:26:04 -0400

    Update edit_transactions.php

--- repo/Project/edit_transactions.php
+++ repo/Project/edit_transactions.php
@@ -1,7 +1,6 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
 if (!has_role("Admin")) {
-    //this will redirect to login and kill the rest of this script (prevent it from executing)
     flash("You don't have permission to access this page");
     die(header("Location: login.php"));
 ?>
@@ -21,7 +20,7 @@ if (isset($_POST["save"])) {
     $user = get_user_id();
     $db = getDB();
 
-    if (isset($id)) { //balance and trasnaction type
+    if (isset($id)) { 
         $stmt = $db->prepare("UPDATE Transactions set amount where id=:id"); //check proper ID
         $r = $stmt->execute([
             ":amount" => $amount,
